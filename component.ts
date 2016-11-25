ab427fad3baab3d0b4e76d7a1b5b376447010dae
diego <diegofsza@gmail.com>
2016-11-25 15:35:15 -0300

    Refactor fix

--- component.ts
+++ component.ts
@@ -1,5 +1,5 @@
 export { TableComponent } from './src/app/components/table/table.component';
-export { FooterComponent } from './src/app/components/footer/footer.component';
+export { TableFooterComponent } from './src/app/components/footer/tablefooter.component';
 export { DataRequestConfig } from './src/app/components/data-request-config';
 export { HeaderItem } from './src/app/components/header-item';
 export { PaginatedResults } from './src/app/components/paginated-results';
