31f057a31c5d2729a115e7ccc78b0fe779757ef9
diegofsza <diegofsza@gmail.com>
2017-04-08 12:25:48 -0300

    Module fix

--- src/app/component-wrapper/src/app/ngx-iq-table.module.ts
+++ src/app/component-wrapper/src/app/ngx-iq-table.module.ts
@@ -2,14 +2,14 @@
  * Created by diego on 4/6/17.
  */
 import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
 import {TableComponent} from './table/table.component';
 import {FooterComponent} from './footer/footer.component';
 import {TableStateService} from './table-state.service';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
     imports: [
-        BrowserModule
+        CommonModule
     ],
     declarations: [
         TableComponent,
